1b81d682bb0e3509522132f6f0448a66782d09e9
saravanan642 <saravanansenthil605@gmail.com>
2026-02-13 16:09:06 +0530

    Header updated

--- src/Compoents/Header.js
+++ src/Compoents/Header.js
@@ -1,22 +1,32 @@
 import React from 'react'
-
+import background from "../Assests/letter-s.png"
 const Header = () => {
   return (
     <div>
 
-      <div>
-        <h1>Saravanan</h1>
+      <div className=' flex   text-white gap-11 justify-around py-3  border-b '>
+        <div className=' flex gap-4 ml-auto border border-none pl-[14%]'>
+          <img src={background} alt='background' className=' w-[10%] rounded-full' ></img>
+          <h1 className=' text-3xl mt-2 '>Saravanan</h1>
+        </div>
 
-        <div className='  text-white    
-        lg:flex gap-8
-        lg"
+        <div className=' mt-4 pr-[7%]    
+        lg:flex gap-20
+        
         ' >
           <a href=''>HOME</a>
           <a href=''>ABOUT</a>
           <a href=''>PROJECTS</a>
           <a href=''> SKILLS</a>
-          <button>Contact Me</button>
+          <a href='' className='  
+          lg:border border-solid
+          lg:pl-3 p-1
+          lg:w-24 rounded-full 
+          lg:relative bottom-2 
+          lg:mt-1
+          lg: bg-blue-600 '>Contents</a>
         </div>
+
 
       </div>
     </div>
