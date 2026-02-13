03ba7da2b36fd011caea2b1719fe0c85d5d494c2
saravanan642 <saravanansenthil605@gmail.com>
2026-02-13 13:59:45 +0530

    starting Home.js

---
+++ src/Compoents/Header.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+
+      <div>
+        <h1>Saravanan</h1>
+
+        <div className='  text-white    
+        lg:flex gap-8
+        lg"
+        ' >
+          <a href=''>HOME</a>
+          <a href=''>ABOUT</a>
+          <a href=''>PROJECTS</a>
+          <a href=''> SKILLS</a>
+          <button>Contact Me</button>
+        </div>
+
+      </div>
+    </div>
+
+  )
+}
+
+export default Header
