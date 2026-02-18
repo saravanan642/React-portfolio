5a1c884da4afd0e63bb41d672c907aec8441f1dd
saravanan642 <saravanansenthil605@gmail.com>
2026-02-18 15:21:15 +0530

    home page upadte

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import Header from "./Compoents/Header";
+import Home from "./Compoents/Home";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Home />
+      
     </div>
   );
 }
---
+++ src/Compoents/Home.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div>
+            <div className=' text-white font-serif'>
+                <p>🖐🏻 Hi, I'm Saravanan</p>
+
+                <div className=' flex gap-3'><h1 className='text-3xl'>Full Stack Developer </h1><h1 className='text-purple-500 text-3xl'>&</h1></div>
+                <h1 className='text-3xl mt-2 text-purple-500 '>Frontend Specialist .</h1>
+                <div className=' mt-5  tracking-wider font-serif'>
+                    <p>AS Full Stack Developer, I build responsive,scalabel,and </p>
+                    <p>user-friendly web application using modern </p>
+                    <p>technologies like React,Node.js and MongoDB</p>
+                </div>
+
+            </div>
+
+        </div>
+    )
+}
+
+export default Home
