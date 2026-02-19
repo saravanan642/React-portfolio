9ff61ca59c664d40e9985226ba50052e5a938e3d
saravanan642 <saravanansenthil605@gmail.com>
2026-02-19 12:11:49 +0530

    complete home page

--- src/Compoents/Home.js
+++ src/Compoents/Home.js
@@ -1,17 +1,17 @@
 import React from "react";
-import profile from "../Assests/My image.png";
+import profile from "../Assests/saravanan.png";
 
 const Home = () => {
   return (
     <section className="min-h-screen flex items-center justify-center px-6 md:px-20 bg-gradient-to-br from-[#12002f] via-[#1f0036] to-[#0b1d5e]">
 
-      <div className="flex flex-col-reverse md:flex-row items-center justify-between gap-12 w-full max-w-6xl">
+      <div className="flex flex-col-reverse md:flex-row items-center justify-between gap-20  w-full max-w-6xl">
 
         {/* Left Content */}
-        <div className="text-white font-serif max-w-3xl text-center md:text-left">
+        <div className="text-white font-serif max-w-3xl text-center md:text-left md:ml-[7%]">
 
           {/* Greeting */}
-          <p className="text-lg md:text-xl mb-4 animate-fadeIn">
+          <p className="text-lg md:text-2xl mb-4 animate-fadeIn">
             🖐🏻 Hi, I'm <span className="text-purple-400 font-semibold">Saravanan</span>
           </p>
 
@@ -59,7 +59,7 @@ const Home = () => {
             <img
               src={profile}
               alt="profile"
-              className="relative w-60 md:w-80 lg:w-96 rounded-full border-4 border-purple-500 shadow-2xl"
+              className="relative w-60 md:w-60 lg:w-80 rounded-full  border-4 border-purple-500 shadow-2xl"
             />
           </div>
         </div>
